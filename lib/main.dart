0996172bb49abaa3fc710de2e735ab8d8bc8edbf
michele <nikola.stanic.official@gmail.com>
2023-06-09 10:24:26 +0200

    Initial commit

---
+++ lib/main.dart
@@ -0,0 +1,21 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_financia/pages/home_page.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'FINANCIA',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: const HomePage(title: 'FINANCIA - HOME'),
+    );
+  }
+}
